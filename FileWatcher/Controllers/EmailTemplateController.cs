643c8ea71b2725973ec299872710bc9df7c341c1
harrish.net9 <harrish.net9@gmail.com>
2024-09-08 00:23:58 +0530

    Added Text Editor for Email templates

---
+++ FileWatcher/Controllers/EmailTemplateController.cs
@@ -0,0 +1,80 @@
+﻿using FileWatcherApp.Models.EmailTemplate;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FileWatcherApp.Controllers
+{
+
+
+    public class EmailTemplateController : Controller
+    {
+        // Replace this with your data access logic
+        private static List<EmailTemplateViewModel> _emailTemplates = new List<EmailTemplateViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(_emailTemplates);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new EmailTemplateViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Create(EmailTemplateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Id = _emailTemplates.Count + 1; // Simplified ID generation
+                _emailTemplates.Add(model);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var template = _emailTemplates.FirstOrDefault(t => t.Id == id);
+            if (template == null)
+            {
+                return NotFound();
+            }
+
+            return View(template);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmailTemplateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var template = _emailTemplates.FirstOrDefault(t => t.Id == model.Id);
+                if (template != null)
+                {
+                    template.TemplateName = model.TemplateName;
+                    template.Subject = model.Subject;
+                    template.EmailBody = model.EmailBody;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var template = _emailTemplates.FirstOrDefault(t => t.Id == id);
+            if (template != null)
+            {
+                _emailTemplates.Remove(template);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+
+}
---
+++ FileWatcher/Models/EmailTemplate/EmailTemplateViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FileWatcherApp.Models.EmailTemplate
+{
+    public class EmailTemplateViewModel
+    {
+        public int Id { get; set; }
+        public string TemplateName { get; set; }
+        public string Subject { get; set; }
+        public string EmailBody { get; set; } // This will store the HTML content
+    }
+}
